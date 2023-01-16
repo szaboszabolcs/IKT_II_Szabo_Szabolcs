7b78c18243322e2b98627299b35a8f1e912c6899
szabolcs <szabosz@kkszki.hu>
2023-01-16 10:14:30 +0100

    CRUD műveletek működnek Microsoft adatbázisban.

--- Movie_Web_API/Movie_Web_API/Controllers/MoviesController.cs
+++ Movie_Web_API/Movie_Web_API/Controllers/MoviesController.cs
@@ -16,7 +16,7 @@ namespace Movie_Web_API.Controllers
             _context = context;
         }
 
-        [HttpGet(Name = "Filmek")]
+        [HttpGet]
 
         public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
         {
@@ -27,6 +27,94 @@ namespace Movie_Web_API.Controllers
             return await _context.Movies.ToListAsync();
         }
 
-        
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (_context.Movies == null)
+            {
+                return NotFound();
+            }
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie), new
+            {
+                id = movie.Id,
+
+            }, movie);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+       
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            if (_context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(long id)
+        {
+            return (_context.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
